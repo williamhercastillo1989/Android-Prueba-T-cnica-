ae34f27884cd0574b87856f53ed3895a08f39b24
william hernandez <williamhercastillo1989@gmail.com>
2025-01-21 05:44:51 -0600

    #Project configuration libraries and dependencies are added #Dagger #Retrofit #Coil #Paging 3

---
+++ app/build.gradle.kts
@@ -0,0 +1,79 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose)
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.example.liverpooltestproductsearch"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.liverpooltestproductsearch"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    val paging_version = "3.3.2"
+    //  Dagger Hilt
+    implementation("com.google.dagger:hilt-android:2.52")
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.paging.common.android)
+    kapt("com.google.dagger:hilt-compiler:2.52")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    //Coil
+    implementation("io.coil-kt:coil-compose:1.4.0")
+
+    // optional - Jetpack Compose integration
+    implementation("androidx.paging:paging-compose:$paging_version")
+
+    implementation("androidx.paging:paging-runtime:$paging_version")
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
